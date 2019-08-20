024bf6f8ba499e011f5fd1e3f88472c50bff7d43
梦 <2584425968@qq.com>
2019-08-20 22:05:38 +0800

    分栏内容

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,3 +1,5 @@
+import request from "../../utils/request.js"
+
 // pages/category/index.js
 Page({
 
@@ -5,62 +7,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    //总的数据
+    navs:[],
+    //索引
+    current:0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //请求菜单栏数据
+    request({
+      url:"/categories"
+    }).then(res=>{
+      //返回的数组
+      const {message}=res.data;
+      //修改数据
+      this.setData({
+        navs:message
+      })
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  //菜单栏点击时触发
+  handleChange(event){
+    const{index}=event.currentTarget.dataset;
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    this.setData({
+      current:index
+    })
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,53 @@
+import request from "../../utils/request.js";
 
 Page({
   data: {
     autoplay: true,
+    // 轮播图的数据
     imgUrls: [
-      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
-      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
-      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+      // 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      // 'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      // 'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
     ],
+    // 储存菜单数据
+    menus: [],
+    // 储存楼层数据
+    floors: []
+  },
+
+  onLoad() {
+    // 请求轮播图数据
+    request({
+      url: "/home/swiperdata"
+    }).then(res => {
+      // 返回的数组
+      const { message } = res.data;
+      // 修改imgUrls
+      this.setData({
+        imgUrls: message
+      })
+    });
+
+    //请求菜单栏数据
+    request({
+      url:"/home/catitems"
+    }).then(res=>{
+      const {message}=res.data;
+
+      this.setData({
+        menus:message
+      })
+    })
+
+    //请求楼层数据
+    request({
+      url:"/home/floordata"
+    }).then(res=>{
+      const {message}=res.data;
+
+      this.setData({
+        floors:message
+      })
+    })
   }
 })
